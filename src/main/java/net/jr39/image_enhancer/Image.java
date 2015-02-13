8981ba12573b4945432ce15180e9431fda59d300
James Ralphs <jr.ralphs@gmail.com>
2015-02-13 21:12:18 +0100

    remove license template

--- src/main/java/net/jr39/image_enhancer/Image.java
+++ src/main/java/net/jr39/image_enhancer/Image.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.jr39.image_enhancer;
 
 import java.awt.image.BufferedImage;
