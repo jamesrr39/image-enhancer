b120243a4a8c8097729cdb020020760b4a64b17a
James Ralphs <jr39.dev@gmail.com>
2015-11-22 20:31:24 +0100

    add example webapp

--- webapp/src/main/java/net/jr39/image_enhancer/webapp/ImageTransformationService.java
+++ webapp/src/main/java/net/jr39/image_enhancer/webapp/ImageTransformationService.java
@@ -1,10 +1,8 @@
 package net.jr39.image_enhancer.webapp;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -15,15 +13,8 @@ import javax.ws.rs.core.Response;
 @Path("/transform")
 public interface ImageTransformationService {
 	
-	@GET
-	@Produces("image/jpg")
-	public Response transformImage();
-	
-	
 	@POST
-	@Produces("image/png")
-	//@Consumes(MediaType.APPLICATION_OCTET_STREAM)
 	@Consumes(MediaType.TEXT_PLAIN)
-	public Response transformPostedImage(String a);
+	public Response transformPostedImage(String imageAsBase64);
 	
 }
--- webapp/src/main/java/net/jr39/image_enhancer/webapp/ImageTransformationServiceImpl.java
+++ webapp/src/main/java/net/jr39/image_enhancer/webapp/ImageTransformationServiceImpl.java
@@ -1,11 +1,11 @@
 package net.jr39.image_enhancer.webapp;
 
-import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -21,37 +21,9 @@ import net.jr39.image_enhancer.transformation_platform.graphics.transformations.
 public class ImageTransformationServiceImpl implements ImageTransformationService {
 
 	@Override
-	public Response transformImage() {
-		Color startColour = Color.MAGENTA;
-
-		final int IMAGE_WIDTH = 200;
-		final int IMAGE_HEIGHT = 100;
-		final int startColourInt = startColour.getRGB();
-
-		BufferedImage image = new BufferedImage(IMAGE_WIDTH, IMAGE_HEIGHT, BufferedImage.TYPE_4BYTE_ABGR);
-		int[] rgbArray = new int[image.getWidth() * image.getHeight()];
-		for (int i = 0; i < rgbArray.length; i++) {
-			rgbArray[i] = startColourInt;
-		}
-		image.setRGB(25, 50, image.getWidth() - 25, image.getHeight() - 50, rgbArray, 0, image.getWidth() - 25);
-
-		SepiaTransformation transformation = new SepiaTransformation(new GenericTransformationArgs(null));
-		BufferedImage transformedImage = transformation.transform(image);
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		try {
-			ImageIO.write(transformedImage, "jpeg", baos);
-		} catch (IOException ex) {
-			throw new RuntimeException(ex);
-		}
-		byte[] imageData = baos.toByteArray();
-
-		return Response.ok(imageData).build();
-	}
-
-	@Override
-	public Response transformPostedImage(String base64Image) {
-		String encodedImage = base64Image.substring(base64Image.indexOf(",") + 1);
+	public Response transformPostedImage(String imageAsBase64) {
+		String prefix = imageAsBase64.substring(0, imageAsBase64.indexOf(",") + 1);
+		String encodedImage = imageAsBase64.substring(imageAsBase64.indexOf(",") + 1);
 		Base64.Decoder decoder = Base64.getDecoder();
 		Base64.Encoder encoder = Base64.getEncoder();
 		byte[] bs = decoder.decode(encodedImage);
@@ -63,7 +35,7 @@ public class ImageTransformationServiceImpl implements ImageTransformationServic
 			BufferedImage transformedImage = transformation.transform(bImageFromConvert);
 			ByteArrayOutputStream baos = new ByteArrayOutputStream();
 			ImageIO.write(transformedImage, "png", baos);
-			return buildCORSResponse(Response.ok(encoder.encode(baos.toByteArray())));
+			return buildCORSResponse(Response.ok(prefix + encoder.encodeToString(baos.toByteArray())));
 		} catch (IOException ex) {
 			String errorMessage = "Couldn't convert base 64 string to image";
 			Logger.getLogger(ImageTransformationServiceImpl.class.getName()).log(Level.SEVERE, errorMessage, ex);
